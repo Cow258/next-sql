e896153c44ff87e37fa01c9734b3e1a2e310a89a
Cowboy Ho <dreamran43@gmail.com>
2021-03-08 17:55:07 +0800

    Update

--- cli/dev.js
+++ cli/dev.js
@@ -1,3 +1,6 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-redeclare */
+/* eslint-disable no-var */
 // const d = new Date('2001-01-01T00:00:00.000Z')
 // console.log(d.getTime())
 const util = require('util')
@@ -52,15 +55,44 @@ const s = sql()
 const { conditions, ...state } = s._state
 console.table('state', state)
 console.table('conditions', conditions)
-console.log('s.getStatement()', s.getStatement())
+const statement = s.getStatement('read', 'users')
+console.log('s.getStatement()', statement)
+console.table('s.getRaw()', s.getRaw(...statement))
+
+console.log('===================================')
+
+const s2 = sql()
+const statement2 = s2.getStatement('update', 'users', {
+  name: 'Mary',
+  cash: 50,
+  createAt: new Date(),
+})
+console.log('s2.getStatement()', statement2)
+console.table('s2.getRaw()', s2.getRaw(...statement2))
+
+// console.log('===================================')
+
+// const s3 = s.clone()
+// s3.and({ asd: 999 })
+// const statement3 = s3.getStatement('update', 'users', { name: 'Mary' })
+// console.log('s3.getStatement()', statement3)
+// console.table('s3.getRaw()', s3.getRaw(...statement3))
+
+// console.log('===================================')
+
+// const statement4 = s.getStatement('read', 'users')
+// console.log('s.getStatement()', statement4)
+// console.table('s.getRaw()', s.getRaw(...statement4))
 
 
-async function main() {
-  const users = await sql().query('SELECT * FROM users')
-  console.log(users)
-  process.exit()
-}
-main()
+// async function main() {
+//   const users = await sql().query('SELECT * FROM users')
+//   console.log(users)
+//   process.exit()
+// }
+// main()
+
+
   // (async () => {
   // })()
 
